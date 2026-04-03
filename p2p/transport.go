7da9e070a218810fb0c7efda764019c84a30d7bc
Keshav Bansal <keshavbansal015@gmail.com>
2026-04-03 09:46:11 -0500

    Learned a bit about testing, and net library

---
+++ p2p/transport.go
@@ -0,0 +1,11 @@
+package p2p
+
+// Represents remote node
+type Peer interface {
+}
+
+// Anything that handles the communication between,
+// the nodes in the network
+// (TCP, UDP, WebSockets, ...)
+type Transport interface {
+}
